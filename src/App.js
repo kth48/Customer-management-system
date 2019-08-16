52c5b02b70a576f6809ee9807062bef13cab7106
kth48 <rlaxogus3617@naver.com>
2019-08-16 22:17:39 +0900

    Create React Project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+      <dir className ="gray-background">
+        <img src ={logo} lat ="logo"/>
+        <h2>Let's develop management system!</h2>
+      </dir>
+  );
+}
+
+export default App;
