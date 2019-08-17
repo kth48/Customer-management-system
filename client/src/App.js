0de67685363d8d0ef2c5eaf831fc6a3bbd433078
kth48 <rlaxogus3617@naver.com>
2019-08-18 00:14:17 +0900

    add Customer Api Loading

--- client/src/App.js
+++ client/src/App.js
@@ -7,52 +7,51 @@ import TableHead from'@material-ui/core/TableHead';
 import TableBody from'@material-ui/core/TableBody';
 import TableRow from'@material-ui/core/TableRow';
 import TableCell from'@material-ui/core/TableCell';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import { withStyles } from '@material-ui/core/styles';
+
 
 
 const styles = theme => ({
   root : {
     width : '100%',
-    marginTop:theme.spacing.unit *3,
+    marginTop:theme.spacing(3),
     overflowX : "auto"
   },
   table :{
     minWidth : 1080
+  },
+  progress:{
+    margin: theme.spacing(2),
   }
-})
+});
 
-
-
-
-
-const customers = [
-{
-  'id' : 1,
-  'image' : 'https://placeimg.com/64/64/1',
-  'name' : '김태현',
-  'birthday' : '961222',
-  'gender' : '남자',
-  'job' : '대학생'
-},
-{
-  'id' : 2,
-  'image' : 'https://placeimg.com/64/64/2',
-  'name' : '이승수',
-  'birthday' : '961111',
-  'gender' : '남자',
-  'job' : '대학생'
-},
-{
-  'id' : 3,
-  'image' : 'https://placeimg.com/64/64/3',
-  'name' : '오지아',
-  'birthday' : '961011',
-  'gender' : '여자',
-  'job' : '대학생'
-}
-]
 
 class App extends Component {
+
+  state = {
+    customers: "",
+    completed: 0
+  }
+
+  componentDidMount(){
+    this.timer = setInterval(this.progress, 20);
+    this.callApi()
+      .then(res => this.setState({customers: res}))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async() => {
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
+  progress = () => {
+    const{completed} =this.state;
+    this.setState({completed:completed >= 100 ? 0 : completed + 1});
+  }
+  
   render() {
     const { classes } = this.props
     return (
@@ -69,7 +68,15 @@ class App extends Component {
               </TableRow>
             </TableHead>
             <TableBody>
-              { customers.map(c => { return( <Customer key = {c.id} id = {c.id} image = {c.image} name = {c.name} birthday = {c.birthday} gender = {c.gender} job = {c.job} /> ); } ) }
+              { this.state.customers ? this.state.customers.map(c => { 
+                return( <Customer key = {c.id} id = {c.id} image = {c.image} name = {c.name} birthday = {c.birthday} gender = {c.gender} job = {c.job} /> ); 
+              }) : 
+              <TableRow>
+                <TableCell colSpan = "6" align = "center">
+                  <CircularProgress className = {classes.progress} variant = "determinate" value = {this.state.completed}/>
+                </TableCell>
+              </TableRow>
+              }
             </TableBody>
           </Table>         
         </Paper>
